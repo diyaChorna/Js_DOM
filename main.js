8f1f0e1499a227081cd00e79425d9135abbc0738
unknown <diana.chorna@gmail.com>
2022-01-23 21:10:21 +0200

    js

---
+++ main.js
@@ -0,0 +1,27 @@
+// 1
+// let colorPage = prompt("Введіть фон для сторінки");
+// document.body.style.background = colorPage;
+
+// 2
+// let fontStyle = prompt("Введіть шрифт для сторінки");
+// document.body.style.fontFamily = fontStyle;
+
+// 3
+// let fontSize = prompt("Введіть розмір шрифту");
+// document.body.style.fontSize = `${fontSize}px`;
+
+// 4
+// let pBackground = prompt("Фон для параграфа з ссилкою");
+// document.getElementById('p').style.background = pBackground;
+
+// 5
+// let pColor = prompt("Колір для параграфа з ссилкою");
+// document.getElementById('p').style.color = pColor;
+
+// 6
+// let divColor = prompt("Колір для тексту div");
+// document.querySelector('div').style.color = divColor;
+
+// 7
+// let divFontSize = prompt("Розмір для тексту div");
+// document.querySelector('div').style.fontSize = `${divFontSize}px`;
